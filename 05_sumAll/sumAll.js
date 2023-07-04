1ec64f4c5e4c5ea973829352461d6d399ffde767
PrachponleuUch <ponleuvuthy@gmail.com>
2023-07-04 23:14:57 +1000

    Add my own solutions

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+  let total = 0;
+  if(min > max){
+    let temp = min;
+    min = max;
+    max = temp;
+  }
+  if ((typeof min) === 'number' && (typeof max) === 'number' && min >= 0 && max >= 0){
+    for(let i=min; i<=max; i++){
+      total += i;
+    }
+  }
+  else{
+    return 'ERROR';
+  }
+  return total;
 };
-
 // Do not edit below this line
 module.exports = sumAll;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let result = (temp - 32) * 5/9;
+  if (Number.isInteger(result) && Number.isInteger(temp)){
+    return (temp - 32) * 5/9;
+  }
+  return Number(parseFloat(result).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let result = (temp * 9/5) + 32;
+  if (Number.isInteger(result) && Number.isInteger(temp)){
+    return result;
+  }
+  return Number(parseFloat(result).toFixed(1));
 };
 
 // Do not edit below this line

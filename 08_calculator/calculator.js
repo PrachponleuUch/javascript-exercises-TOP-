cfe046bc931f6c459a274334f6a1e8a14a9d56ce
PrachponleuUch <ponleuvuthy@gmail.com>
2023-07-09 17:12:21 +1000

    Add solution to 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return (arr.length != 0) ? arr.reduce((ttl, num) => ttl + num): 0;
 };
 
-const multiply = function() {
-
+const multiply = function(...args) {
+  let ttl = 1;
+  args.forEach((arg) => ttl *= arg);
+  return ttl;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let ttl = 1;
+	if (a === 0)
+  {
+    return 1;
+  } 
+  else {
+    for (let i = 1; i < a + 1; i++){
+      ttl *= i
+    }
+    return ttl;
+  }
 };
 
 // Do not edit below this line
